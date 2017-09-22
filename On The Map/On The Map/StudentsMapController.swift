8e3cc0f33380b8a88880e60b68a0944db50d1904
Jason Wedepohl <jason.wedepohl@gmail.com>
2017-09-22 15:20:01 +0200

    Map view with student annotations

--- On The Map/On The Map/StudentsMapController.swift
+++ On The Map/On The Map/StudentsMapController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import MapKit
 
-class StudentsMapController: UIViewController {
+class StudentsMapController: UIViewController, MKMapViewDelegate {
+	
+	//MARK: Constants
+	
+	let pinReuseID = "Pin"
+	
+	//MARK: Outlets
+	
+	@IBOutlet weak var mapView: MKMapView!
+	
+	//MARK: UIViewController overrides
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
@@ -16,5 +27,55 @@ class StudentsMapController: UIViewController {
 	
 	func displayStudentRecords() {
 		print("Displaying student records on map.")
+		
+		//remove old annotations from the map
+		let oldAnnotations = mapView.annotations
+		mapView.removeAnnotations(oldAnnotations)
+		
+		//create new annotations
+		var annotations = [MKPointAnnotation]()
+		
+		for record in ParseClient.shared.studentRecords {
+			let lat = CLLocationDegrees(record.latitude)
+			let long = CLLocationDegrees(record.longitude)
+			
+			let annotation = MKPointAnnotation()
+			annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+			annotation.title = "\(record.firstName) \(record.lastName)"
+			annotation.subtitle = record.mediaURL
+			
+			annotations.append(annotation)
+		}
+		
+		//add annotations to map
+		mapView.addAnnotations(annotations)
+	}
+	
+	//MARK: MKMapViewDelegate implementation
+	
+	// Create annotation view
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: pinReuseID) as? MKPinAnnotationView
+		
+		if pinView == nil {
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: pinReuseID)
+			pinView!.canShowCallout = true
+			pinView!.pinTintColor = .red
+			pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+		}
+		else {
+			pinView!.annotation = annotation
+		}
+		
+		return pinView
+	}
+	
+	//Open browser to annotation link when user taps annotation
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+		if control == view.rightCalloutAccessoryView {
+			if let mediaUrl = view.annotation?.subtitle! {
+				Utilities.openURL(mediaUrl)
+			}
+		}
 	}
 }
